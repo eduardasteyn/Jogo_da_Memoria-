09f9d61afbea2c92ef0cb267426680ac7580a04a
Eduarda Gislon <Eduarda.jen@gmail.com>
2024-02-06 20:46:39 -0300

    View Builder e private
    feat(Cardify):  Nova struct do tipo viewBuilder para modificar a view quando o card está virado para cima ou baixo
    perf(EmojiMemoryGame): privatizando a função createMemoryGame para que nenhuma outra classe possa utilizar ela e assim dando mais segurança a classe
    perf(Grid): privatizando a variável items e a variável viewForItem.
    perf(GridLayout): privatizando a variável size, rowCount e a variável columnCount.
    perf(MemoryGame): privatizando a variável cards, onde podemos ainda ver seu conteúdo, mas não podemos editar ele.
    feat(MemoryGameView): Com a criação da Cardify, a view teve algumas limpeza de código e foi implementado o fundo de torta nos cards.
    feat(Pie): A classe cria um fundo de torta de acordo com o ângulo recebido, podendo ser reutilizado varias vezes.

---
+++ JogoDaMemoria/JogoDaMemoria/Cardify.swift
@@ -0,0 +1,36 @@
+//
+//  Cardify.swift
+//  JogoDaMemoria
+//
+//  Created by Eduarda Gislon on 06/02/24.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: cornerRadius).fill(.white)
+                RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
+                content
+            } else {
+                RoundedRectangle(cornerRadius: cornerRadius).fill()
+            }
+        }
+    }
+
+// MARK: - Drawing Constants
+private let cornerRadius: CGFloat = 10.0
+private let edgeLineWidth: CGFloat = 3
+    
+}
+
+extension View {
+    func cardify(isFaceUp: Bool) -> some View {
+        self.modifier(Cardify(isFaceUp: isFaceUp))
+    }
+}
