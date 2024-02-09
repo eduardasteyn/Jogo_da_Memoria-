9c41c1977306eafbaa2de52a3f536d2f97a54963
Eduarda Gislon <Eduarda.jen@gmail.com>
2024-02-08 23:47:20 -0300

    Implementação de animações feat(Cardify): Alterado o tipo da struct para AnimatableModiffier para que a animação possa ser realizada refactor(Cardify): Adicionado novas constants para que o código fique mais legível feat(EmojiMemoryGame): Adicionado novo intent para resetar o jogo feat(MemoryGame): A modelo recebeu novas variáveis para manipular melhor o tempo de animação ou tempo de jogo refactor(MemoryGameView): Adicionado novas constantes para os valores numéricos feat(MemoryGameView): Adicionado novas animação de transição feat(Pie): Implementado animação para que agora a nossa torta tenha um temporizador que vai diminuindo seu ângulo

--- JogoDaMemoria/JogoDaMemoria/Cardify.swift
+++ JogoDaMemoria/JogoDaMemoria/Cardify.swift
@@ -7,25 +7,47 @@
 
 import SwiftUI
 
-struct Cardify: ViewModifier {
+struct Cardify: AnimatableModifier {
     
-    var isFaceUp: Bool
+    var rotation: Double
+    var isFaceUp: Bool {
+        rotation < angle
+    }
+    
+    init(isFaceUp: Bool) {
+        rotation = isFaceUp ? 0 : 180
+    }
+    
+    var animatableData: Double {
+        get { return rotation }
+        set { rotation = newValue}
+    }
     
     func body(content: Content) -> some View {
         ZStack {
-            if isFaceUp {
+            Group {
                 RoundedRectangle(cornerRadius: cornerRadius).fill(.white)
                 RoundedRectangle(cornerRadius: cornerRadius).stroke(lineWidth: edgeLineWidth)
                 content
-            } else {
-                RoundedRectangle(cornerRadius: cornerRadius).fill()
             }
+                .opacity(opacity(isFaceUp, isBefore: true))
+            RoundedRectangle(cornerRadius: cornerRadius).fill()
+                .opacity(opacity(isFaceUp, isBefore: false))
         }
+        .rotation3DEffect(Angle.degrees(rotation), axis: axis)
+
     }
 
-// MARK: - Drawing Constants
-private let cornerRadius: CGFloat = 10.0
-private let edgeLineWidth: CGFloat = 3
+    // MARK: - Drawing Constants
+    private let cornerRadius: CGFloat = 10.0
+    private let edgeLineWidth: CGFloat = 3
+    private let angle: Double = 90
+    private let axis: (x: CGFloat, y: CGFloat, z: CGFloat) = (0,1,0)
+    
+    private func opacity(_ isFaceUp: Bool, isBefore: Bool) -> CGFloat {
+        isBefore ? isFaceUp ? 1 : 0 : isFaceUp ? 0 : 1
+    }
+
     
 }
 
